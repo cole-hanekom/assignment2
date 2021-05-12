dae53313de35e79d6d98e7850ae8b7e1b3d5dd9f
Cole Hanekom <217267556@mycput.ac.za>
2021-05-12 13:38:31 +0000

    Added code for Collection in main class and test code in test class

--- src/main/java/za/ac/cput/CollectionInterfaces.java
+++ src/main/java/za/ac/cput/CollectionInterfaces.java
@@ -1,7 +1,47 @@
 package za.ac.cput;
 
+/**
+ * @Author: Cole Hanekom
+ * @Student Number: 217267556
+ * Main Class
+ * @Date: 16/05/2021
+ */
+
+import java.util.*;
+
 public class CollectionInterfaces {
+
     public static void main(String[] args) {
+        //Collection
+        System.out.println( "Collection Interface"  );
+        // creating an empty LinkedList
+        Collection<String> teamList = new LinkedList<String>();
+
+        // use add() method to add elements in the list
+        teamList.add("Manchester United");
+        teamList.add("Arsenal");
+        teamList.add("Chelsea");
+        teamList.add("Liverpool");
+
+        // Output the present list
+        System.out.println("The list is: " + teamList);
+
+        //adding a new element to the list
+        teamList.add("Leeds");
+        System.out.println("The list after adding an item is: " + teamList);
+
+        //removing an element from the list
+        teamList.remove("Liverpool");
+        System.out.println("The list after removing an item is: " + teamList);
+
+        //Checking if an item can be found in the list
+        // using contains() method
+        boolean result = teamList.contains("Chelsea");
+
+        // printing the result
+        System.out.println("Is Chelsea present in the list: "
+                + result);
+
 
     }
 }
--- src/test/java/za/ac/cput/CollectionInterfacesTest.java
+++ src/test/java/za/ac/cput/CollectionInterfacesTest.java
@@ -1,7 +1,51 @@
 package za.ac.cput;
 
+/**
+ * @Author: Cole Hanekom
+ * @Student Number: 217267556
+ * Test Class
+ * @Date: 16/05/2021
+ */
+
+import org.junit.jupiter.api.Test;
+import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CollectionInterfacesTest {
+    // Testing Collection Interface
+    //Testing to add item
+    @Test
+    void collectionAdd() {
+        Collection<String> teamList = new LinkedList<String>();
+        teamList.add("Manchester United");
+        teamList.add("Arsenal");
+        teamList.add("Chelsea");
+        teamList.add("Liverpool");
+        assertEquals(4, teamList.size());
+        teamList.add("Leeds");  //adding another
+        assertEquals(5, teamList.size()); //checking if it was added
+    }
 
+    //Testing to remove item
+    @Test
+    void collectionRemove() {
+        Collection<String> teamList = new LinkedList<String>();
+        teamList.add("Manchester United");
+        teamList.add("Arsenal");
+        teamList.add("Chelsea");
+        teamList.add("Liverpool");
+        teamList.remove("Arsenal");
+        assertEquals(3, teamList.size()); //checking if it was removed
+    }
+
+    //Testing if an item can be found in the list
+    @Test
+    void collectionFind() {
+        Collection<String> teamList = new LinkedList<String>();
+        teamList.add("Manchester United");
+        teamList.add("Arsenal");
+        teamList.add("Chelsea");
+        teamList.add("Liverpool");
+        assertTrue(teamList.contains("Manchester United")); //checking if it was found
+    }
 }
